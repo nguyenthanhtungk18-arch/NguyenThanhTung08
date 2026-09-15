14107d676568e7499ca942db42ccbaac8692cb76
nguyenthanhtungk18-arch <nguyenthanhtungk18@siu.edu.vn>
2026-09-15 16:33:09 +0700

    Create tung.cpp

---
+++ tung.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+
+    cout << "Nhap ten: ";
+    cin >> name;
+
+    cout << "Nhap tuoi: ";
+    cin >> age;
+
+    cout << "\nThong tin sinh vien:\n";
+    cout << "Ten: " << name << endl;
+    cout << "Tuoi: " << age << endl;
+
+    return 0;
+}
